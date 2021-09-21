9dab1e482b14d4be661a04d88c244e62942a6214
maryamsoltani <soltani.maryam@gmail.com>
2021-09-20 21:37:09 -0700

    Add files via upload

---
+++ Dom-exercise.js
@@ -0,0 +1,8 @@
+document.getElementById("container");
+document.querySelector("container");
+document.selectElemntsByClassName("second")
+document.querySelector("ol.third")
+const foundSection = document.querySelector("#container")
+foundSection.innerText = "Hello"
+footer.className += "main";
+const remove 
